26bee3d49be65a32588569edaed9d4b658d63d07
zhirep <pelayoz@mymacewan.ca>
2026-02-14 14:58:54 -0700

    updated

--- js/script.js
+++ js/script.js
@@ -51,40 +51,49 @@ function resetUI() {
 function calculateBooking() {
     let subtotal = 0;
 
-    const name = document.getElementById('name').value;
-    const email = document.getElementById('email').value;
-    const route = document.getElementById('route').value;
-    const numOfTrav = document.getElementById('numberOfTrav').value;
+    const name = document.getElementById('name');
+    const email = document.getElementById('email');
+    const route = document.getElementById('route');
+    const numOfTrav = document.getElementById('numberOfTrav');
     const paymentOpt = document.querySelector('input[name="payment"]:checked');
 
-    if (name == "") {
-        alert("Please fill out all fields.");
+    clearError(name);
+    clearError(email);
+    clearError(route);
+    clearError(numOfTrav);
+
+    if (name.value == "") {
+        alert("Please fill out name.");
+        errorMsg(name);
         return;
     }
 
     const emailPattern = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
-    if (!emailPattern.test(email)) {
+    if (!emailPattern.test(email.value)) {
         alert("Please enter a valid email.");
+        errorMsg(email);
         return;
     }
 
-    if (route == "") {
+    if (route.value == "") {
         alert("Please select a route.");
+        errorMsg(route);
         return;
     }
 
-    if (numOfTrav >= 10 || numOfTrav <= 0) {
+    if (numOfTrav.value >= 10 || numOfTrav.value <= 0) {
         alert("Please enter a valid number between 1-10.");
+        errorMsg(numOfTrav);
         return;
-    } 
+    }
 
     if (!paymentOpt) {
         alert("Please select a payment option.");
         return;
     }
 
-    let routePrice = checkPrice(route);
-    subtotal = routePrice * numOfTrav;
+    let routePrice = checkPrice(route.value);
+    subtotal = routePrice * numOfTrav.value;
 
     const student = document.getElementById('studentBox').checked;
     subtotal = isStudent(student, subtotal);
@@ -93,7 +102,7 @@ function calculateBooking() {
     let total = subtotal + taxableAmount;
 
     updatePrice(total);
-    successMsg(name, numOfTrav, route, email);
+    successMsg(name.value, numOfTrav.value, route.value, email.value);
 }
 
 function updatePrice(value) {
@@ -140,51 +149,98 @@ function isStudent(student, subtotal) {
 function successMsg(name, numberOfTrav, route, email) {
     alert(`Thank you ${name}! Your booking for ${numberOfTrav} people to ${route} has been received. 
 A confirmation email has been sent to ${email}.`);
+
+    let form = document.getElementById("form-grid");
+    form.style.display="none";
 }
 
 function checkContact() {
-    const name = document.getElementById('name').value;
-    const email = document.getElementById('email').value;
-    const number = document.getElementById('phone').value;
+    const name = document.getElementById('name');
+    const email = document.getElementById('email');
+    const number = document.getElementById('phone');
     const messageText = document.getElementById('message');
 
-    if (name == "") {
-        alert("Please fill out all fields.");
+    clearError(name);
+    clearError(email);
+    clearError(number);
+    clearError(messageText);
+
+    if (name.value == "") {
+        alert("Please fill out name.");
+        errorMsg(name);
         return;
     }
 
     const emailPattern = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
-    if (!emailPattern.test(email)) {
+    if (!emailPattern.test(email.value)) {
         alert("Please enter a valid email.");
+        errorMsg(email);
         return;
     }
 
     const phonePattern = /^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$/;
-    if (!phonePattern.test(number)) {
+    if (!phonePattern.test(number.value)) {
         alert("Please enter a valid phone number.");
+        errorMsg(number);
         return;
     }
 
     if (messageText.value == "") {
         alert("Please write your inquiry.");
+        errorMsg(messageText);
         return;
     }
 
-    alert(`Thank you ${name}, we will contact you soon!`);
+    alert(`Thank you ${name.value}, we will contact you soon!`);
+    let form = document.getElementById("form-grid");
+    form.style.display="none";
+}
+
+function expandThumb(thumb, thumbs) {
+    for (let t of thumbs) {
+        t.classList.remove("expanded");
+    }
+
+    thumb.classList.add("expanded");
+}
+
+function errorMsg(element) {
+    element.classList.add("error-border");
+}
+
+function clearError(element) {
+    element.classList.remove("error-border");
 }
 
 function main() {
-    loadWeather();
-    resetUI();
-
-    document.getElementById("submit").addEventListener("click", (event) => {
-        event.preventDefault();
-        calculateBooking();
-    });
-
-    document.getElementById("reset").addEventListener("click", () => {
+    const displayArea = document.getElementById("display");
+    if (displayArea) {
         resetUI();
-    });
+    }
+
+    const submitBtn = document.getElementById("submit");
+    if (submitBtn) {
+        submitBtn.addEventListener("click", (event) => {
+            event.preventDefault();
+            calculateBooking();
+        });
+    }
+
+    const resetBtn = document.getElementById("reset");
+    if (resetBtn) {
+        resetBtn.addEventListener("click", resetUI);
+    }
+
+    const thumbs = document.querySelectorAll(".thumb");
+    for (let t of thumbs) {
+        t.addEventListener("mouseover", () => {
+            expandThumb(t, thumbs);
+        });
+    }
+
+    if (document.getElementById("weatherBox")) {
+        loadWeather();
+    }
 }
 
 main();
